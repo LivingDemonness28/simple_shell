fc9cee038b10994c73ec990d7fc26e90a9c3c8db
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-20 00:52:45 +0200

    function_1.c: _strncat function updated 2nd

--- function_1.c
+++ function_1.c
@@ -1,12 +1,12 @@
 #include "simpleshell.h"
 
 /**
- * _strncat - concatenate two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
- * Return: dest
+* _strncat – concatenate two strings
+* using at most n bytes from src
+* @dest: destination string where the result will be stored
+* @src: the source string to be appended to the destination string.
+*@n: the maximum number of bytes from src to concatenate.
+* Return: A pointer to the resulting concatenated string (dest).
 */
 char *_strncat(char *dest, char *src, int n)
 {
