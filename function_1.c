a8eae4dbf627ceb8528def2795eff9ab9ce1d546
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-20 00:54:06 +0200

    function_1.c: _strncat function updated 3rd

--- function_1.c
+++ function_1.c
@@ -5,7 +5,7 @@
 * using at most n bytes from src
 * @dest: destination string where the result will be stored
 * @src: the source string to be appended to the destination string.
-*@n: the maximum number of bytes from src to concatenate.
+* @n: the maximum number of bytes from src to concatenate.
 * Return: A pointer to the resulting concatenated string (dest).
 */
 char *_strncat(char *dest, char *src, int n)
