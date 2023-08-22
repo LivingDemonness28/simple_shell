c8988e82ac3927b38feee3a8095b85b602b42ef1
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-22 22:52:37 +0200

    functions_6.c: _exit function added

--- functions_6.c
+++ functions_6.c
@@ -59,3 +59,29 @@ return (val_ptr);
 }
 return (NULL);
 }
+
+/**
+ * _exit - exits shell.
+ * @ss_info - simpleshell_t struct param.
+ * Return: exit with exit code.
+*/
+int _exit(simpleshell_t *ss_info)
+{
+int exit_code;
+if (ss_info->av[1])
+{
+exit_code = _eatoi(ss_info->av[1]);
+if (exit_code == -1)
+{
+ss_info->last_cmd_status = 2;
+_eprint(ss_info, "Invalid numeric argument: ");
+_w_str_stdder(ss_info->av[1]);
+_w_stdder('\n');
+return 1;
+}
+ss_info->exit_error = exit_code;
+return -2;
+}
+ss_info->exit_error = -1;
+return -2;
+}
