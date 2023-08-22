4ce29492611aca5695ccbc81b492e627d4e29f9a
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-22 20:48:54 +0200

    simpleshell.h updated 34th

--- simpleshell.h
+++ simpleshell.h
@@ -2,15 +2,8 @@
 #define _SIMPLESHELL_H_
 
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
+#include <stdlib.h>
 #include <stddef.h>
 
 extern char **environ;
