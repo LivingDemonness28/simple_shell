72912933ab863bdcaffa931fb389cf7e395f99ba
Queenbgr <barengramao274@gmail.com>
2023-08-22 00:57:08 -0700

    simple_shell

---
+++ functions_2.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+
+
+/**
+* _strchr – finds a character in a string.
+* @str: string to be searched.
+* @c: character to locate.
+* Return: a pointer to the first occurrence of c in str (Success). NULL (Otherwise)
+* /
+char *_strchr(char *str, char c)
+{
+	for (; *s != ‘\0’; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (NULL);
+}
