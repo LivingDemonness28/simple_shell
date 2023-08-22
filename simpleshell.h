8a51543198106c2e8fbdd37c75c5696508806ce4
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-22 14:51:03 +0200

    simpleshell.h updated 2nd

--- simpleshell.h
+++ simpleshell.h
@@ -1,6 +1,10 @@
 #ifndef _SIMPLESHELL_H_
 #define _SIMPLESHELL_H_
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 /*Function prototypes*/
 char *_strncat(char *dest, char *src, int n);
 
