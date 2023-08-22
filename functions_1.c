5027c9041a32b8d01d4801ff39b949d33ad14207
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-22 21:14:34 +0200

    functions_1.c: _putchar function added

--- functions_1.c
+++ functions_1.c
@@ -70,3 +70,23 @@ free(*ostr);
 *ostr = nstr;
 return (1);
 }
+
+/**
+* _putchar – writes char c to standard output.
+* @c: The char.
+* Return: 1 (Success), -1 (Otherwise)
+*/
+int _putchar(char c)
+{
+static int a;
+static char buffer[1024];
+
+if (c == -1 || a >= 1024)
+{
+write(1, buffer, a);
+}
+if (c != -1)
+buffer[a++] = c;
+
+return (1);
+}
