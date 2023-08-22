a93c964c8877c9b4993c73864f8057eb84d9a862
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-22 16:30:22 +0200

    functions_3.c: _strdup function added

--- functions_3.c
+++ functions_3.c
@@ -98,3 +98,38 @@ _putchar(str[a]);
 a++;
 }
 }
+
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ * Return: pointer to the duplicated string,
+ * or NULL if insufficient memory
+ */
+char *_strdup(char *str)
+{
+char *dup;
+int length = 0, i;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+while (str[length] != '\0')
+{
+length++;
+}
+
+dup = malloc(sizeof(char) * (length + 1));
+
+if (dup == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i <= length; i++)
+{
+dup[i] = str[i];
+}
+return (dup);
+}
