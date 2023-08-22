c43e12800b204bc1a57be62d13ba6cc99604cf37
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-22 22:20:03 +0200

    functions_6.c: _r_buffer function added

--- functions_6.c
+++ functions_6.c
@@ -18,3 +18,24 @@ free(*strarr);
 }
 free(curr_ptr);
 }
+
+/**
+ * _r_buffer - reads data into a buffer
+ * from a specified fd
+ * @ss_info: simpleshell_t struct param.
+ * @buffer: The buffer.
+ * @size: size of buffer.
+ * Retrn: num of bytes read (Success), -1 (Otherwise)
+*/
+ssize_t _r_buffer(simpleshell_t *ss_info, char *buffer, size_t *size)
+{
+ssize_t rbytes = read(ss_info->rfd, buffer, 1024);
+
+if (*size)
+return (0);
+
+if (rbytes >= 0)
+*size = (size_t)rbytes;
+
+return (rbytes);
+}
