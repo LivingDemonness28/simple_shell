ef56af15ba896b32ba90a40385ad3f6e38181a1e
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-22 14:48:14 +0200

    function_2.c: _strtchr function updated 2nd

--- functions_2.c
+++ functions_2.c
@@ -1,18 +1,17 @@
 #include <stddef.h>
 
-
 /**
 * _strchr – finds a character in a string.
 * @str: string to be searched.
 * @c: character to locate.
 * Return: a pointer to the first occurrence of c in str (Success). NULL (Otherwise)
-* /
+*/
 char *_strchr(char *str, char c)
 {
-	for (; *s != ‘\0’; s++)
-	{
-		if (*s == c)
-			return (s);
-	}
-	return (NULL);
+for (; *s != '\0'; s++)
+{
+if (*s == c)
+return (s);
+}
+return (NULL);
 }
