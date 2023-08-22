6a5284575aea6f8b3330b1b49a1cbc737350c4a6
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-23 01:12:15 +0200

    simpleshell.h updated 41st

--- simpleshell.h
+++ simpleshell.h
@@ -113,5 +113,7 @@ ssize_t _r_buffer(simpleshell_t *ss_info, char *buffer, size_t *size);
 char *_envval(simpleshell_t *ss_info, const char *envvar);
 int _exit_shell(simpleshell_t *ss_info);
 void _eval_chain(simpleshell_t *ss_info, char *buffer, size_t *pos, size_t start, size_t length);
+/*functions_7.c*/
+int _chain_del(simpleshell_t *ss_info, char *buffer, size_t *pos)
 
 #endif
