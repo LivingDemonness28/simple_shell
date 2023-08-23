8e874e9b4b703c1cc95c3314972202ebeadc0064
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-23 03:18:15 +0200

    functions_8.c: _append_node function added

--- functions_8.c
+++ functions_8.c
@@ -21,3 +21,48 @@ free(n);
 }
 *hp = NULL;
 }
+
+/**
+ * _append_node - adds node to end of list.
+ * @h: head of list.
+ * @str: string field of new node.
+ * @num: node index.
+ * Return: ptr to new node (Success), NULL (Otherwise).
+*/
+list_t *_append_node(list_t **h, const char *str, int num)
+{
+list_t *nn = malloc(sizeof(list_t)), *n = *h;
+
+if (!h)
+return NULL;
+
+if (!nn)
+return NULL;
+
+_memset((void *)nn, 0, sizeof(list_t));
+nn->num = num;
+
+if (str)
+{
+nn->str = _strdup(str);
+if (!nn->str)
+{
+free(nn);
+return (NULL);
+}
+}
+
+if (n)
+{
+for (; n->next; n = n->next)
+{
+}
+n->next = nn;
+}
+else
+{
+*h = nn;
+}
+
+return (nn);
+}
