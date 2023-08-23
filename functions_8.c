c6cc22bb64df5170e7176b4076c9fed894a1bc34
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-23 03:24:06 +0200

    functions_8.c: _unshift_node function added

--- functions_8.c
+++ functions_8.c
@@ -66,3 +66,37 @@ else
 
 return (nn);
 }
+
+/**
+ * _unshift_node - adds node to start of list.
+ * @h: head of list.
+ * @str: string field of new node.
+ * @num: node index.
+ * Return: ptr to new node (Success), NULL (Otherwise).
+*/
+list_t *_unshift_node(list_t **h, const char *str, int num)
+{
+list_t *nh = malloc(sizeof(list_t));
+
+if (!h)
+return (NULL);
+
+if (!nh)
+return (NULL);
+
+_memset((void *)nh, 0, sizeof(list_t));
+nh->num = num;
+if (str)
+{
+nh->str = _strdup(str);
+if (!nh->str)
+{
+free(nh);
+return (NULL);
+}
+}
+
+nh->next = *h;
+*h = nh;
+return (nh);
+}
