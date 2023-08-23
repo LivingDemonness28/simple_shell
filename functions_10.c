a4f9f41afddcd0035940ec4fd8de83987f631e38
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-23 05:15:40 +0200

    functions_10.c: _strtow_1 function added

--- functions_10.c
+++ functions_10.c
@@ -60,3 +60,73 @@ _del_node_at_ind(&(ss_info->cmd_hist), 0);
 _updt_hist(ss_info);
 return (ss_info->history_count);
 }
+
+/**
+ * _strtow_1 - splits a string into words.
+ * Repeated delimiters are ignored.
+ * @str: input string.
+ * @del: delimiter string.
+ * Return: ptr to arr of str (Success), NULL (Otherwise).
+*/
+char **strtow_1(char *str, char *del)
+{
+int a = 0, b = 0, c, d, word_count = 0;
+char **s;
+
+if (str == NULL || str[0] == 0)
+return NULL;
+
+if (!del)
+del = " ";
+
+while (str[a] != '\0')
+{
+if (!_check_del(str[a], del) && (_check_del(str[a + 1], del) || !str[a + 1]))
+word_count++;
+a++;
+}
+
+if (word_count == 0)
+return NULL;
+
+s = malloc((1 + word_count) * sizeof(char *));
+if (!s)
+return NULL;
+
+a = 0;
+b = 0;
+while (b < word_count)
+{
+while (_check_del(str[a], del))
+a++;
+
+c = 0;
+while (!_check_del(str[a + c], del) && str[a + c])
+c++;
+
+s[b] = malloc((c + 1) * sizeof(char));
+if (!s[b])
+{
+d = 0;
+while (d < b)
+{
+free(s[d]);
+d++;
+}
+free(s);
+return NULL;
+}
+
+d = 0;
+while (d < c)
+{
+s[b][d] = str[a];
+a++;
+d++;
+}
+s[b][d] = 0;
+b++;
+}
+s[b] = NULL;
+return s;
+}
