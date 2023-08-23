89670051c5db19daaaaa43bdb9eddc0c6d5b2c7d
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-23 03:20:24 +0200

    simpleshell.h updated 47th

--- simpleshell.h
+++ simpleshell.h
@@ -121,6 +121,7 @@ size_t _print_str_ele(const list_t *head);
 int _del_node_at_ind(list_t **h, unsigned int ind);
 /*functions_8.c*/
 void _clear_list(list_t **hp);
-list_t *append_node(list_t **h, const char *str, int num);
+list_t *_append_node(list_t **h, const char *str, int num);
+list_t *_unshift_node(list_t **h, const char *str, int num);
 
 #endif
