0930dd0822f036f5e348f656b3653916b1f41abb
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-24 16:01:33 +0200

    functions_14.c: _clear_info function added

--- functions_14.c
+++ functions_14.c
@@ -31,3 +31,35 @@ int _hprint(simpleshell_t *ss_info)
 _lprint(ss_info->cmd_hist);
 return (0);
 }
+
+/**
+ * _clear_info - clears and deallocates memory
+ * used by the simpleshell_t struct.
+ * @ss_info: simpleshell_t struct param.
+ * @ff: true if all fields are freed.
+ * Return: Nothing.
+*/
+void _clear_info(simpleshell_t *ss_info, int ff)
+{
+_free_strarr(ss_info->av);
+ss_info->av = NULL;
+ss_info->cmd_path = NULL;
+
+if (ff)
+{
+if (!ss_info->command_buf)
+free(ss_info->input_args);
+if (ss_info->local_env_list)
+_clear_list(&(ss_info->local_env_list));
+if (ss_info->cmd_hist)
+_clear_list(&(ss_info->cmd_hist));
+if (ss_info->alias_node)
+_clear_list(&(ss_info->alias_node));
+_free_strarr(ss_info->custom_env);
+ss_info->custom_env = NULL;
+_free_ptr((void **)ss_info->command_buf);
+if (ss_info->rfd > 2)
+close(ss_info->rfd);
+_putchar(-1);
+}
+}
