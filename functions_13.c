aa2735dea58b5c70c1db83e005bcd491aaa14fa7
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-24 05:21:25 +0200

    functions_13.c file updated

--- functions_13.c
+++ functions_13.c
@@ -29,8 +29,8 @@ int _my_updt_env(simpleshell_t *ss_info)
 {
 if (ss_info->ac != 3)
 {
-_w_str_stderr("Insufficient arguments\n")
-return (1)
+_w_str_stderr("Insufficient arguments\n");
+return (1);
 }
 if (_updt_env(ss_info, ss_info->av[1], ss_info->av[2]))
 return (0);
