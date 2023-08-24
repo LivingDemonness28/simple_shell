d873f995dd8e33cbc396a68d766212ff91f54ac0
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-24 05:13:28 +0200

    functions_13.c: _my_del_env function added

--- functions_13.c
+++ functions_13.c
@@ -1,1 +1,21 @@
 #include "simpleshell.h"
+
+/**
+ * _my_del_env - remove an env var.
+ * @ss_info: simpleshell_t struct param.
+ * Return: 0 (Always)
+*/
+int _my_del_env(simpleshell_t *ss_info)
+{
+int a = 1;
+
+if (ss_info->ac == 1)
+{
+_w_str_stderr("Insufficient arguments \n");
+return (1);
+}
+for (; a <= ss_info->ac; a++)
+_del_env(ss_info, ss_info->av[a]);
+
+return (0);
+}
