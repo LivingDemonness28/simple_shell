29fa8df51abca94cd3243e196d6a953d30b6d2c4
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-24 05:17:01 +0200

    functions_13.c: _my_updtl_env function added

--- functions_13.c
+++ functions_13.c
@@ -19,3 +19,20 @@ _del_env(ss_info, ss_info->av[a]);
 
 return (0);
 }
+
+/**
+ * _my_updt_env - add or mod env var.
+ * @ss_info: simpleshell_t struct param.
+ * Return: 0 (Always)
+*/
+int _my_updt_env(simpleshell_t *ss_info)
+{
+if (ss_info->ac != 3)
+{
+_w_str_stderr("Insufficient arguments\n")
+return (1)
+}
+if (_updt_env(ss_info, ss_info->av[1], ss_info->av[2]))
+return (0);
+return (1);
+}
