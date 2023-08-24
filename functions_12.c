d2065bb851b56265bbd9cce2f1da5216f8398fbb
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-24 04:20:22 +0200

    functions_12.c: _copy_env function added

--- functions_12.c
+++ functions_12.c
@@ -1,1 +1,16 @@
 #include "simpleshell.h"
+
+/**
+ * _copy_env - str copy of env.
+ * @ss_info: simpleshell_t struct param.
+ * Return: 0 (Always)
+*/
+char **_copy_env(simpleshell_t *ss_info)
+{
+if (ss_info->custom_env || ss_info->env_mod)
+{
+ss_info->custom_env = _strarr(ss_info->local_env_list);
+ss_info->env_mod = 0;
+}
+return (ss_info->custom_env);
+}
