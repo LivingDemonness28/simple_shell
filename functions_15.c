4a39a61101c74a2aa02781ecc9d0f5f1ab2bed61
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-24 17:50:41 +0200

    functions_15.c: _r_line function added

--- functions_15.c
+++ functions_15.c
@@ -1,1 +1,47 @@
 #include "simpleshell.h"
+
+/**
+ * _r_line - reads a line from the input and
+ * appends it to the provided buffer.
+ * @ss_info: simpleshell_t struct param.
+ * @pointer: pointer to buffer.
+ * @curr_len: current length of buffer.
+ * Return: num of chars read and new line (Success), -1 (Otherwise)
+*/
+int _r_line(simpleshell_t *ss_info, char **pointer, size_t *curr_len)
+{
+static char buffer[1024];
+static size_t a, length;
+size_t b;
+ssize_t d = 0, e = 0;
+char *f = f = *pointer, *np = NULL, *c;
+
+if (f && curr_len)
+e = *curr_len;
+if (a == length)
+a = length = 0;
+
+d = _r_buffer(ss_info, buffer, &length);
+if (d == -1 || (d == 0 && length == 0))
+return (-1);
+
+c = _strchr(buffer + a, '\n');
+b = c ? 1 + (unsigned int)(c - buffer) : length;
+np = _realloc(f, e, e ? e + b : b + 1);
+if (!np)
+return (f ? free(f), -1 : -1);
+
+if (e)
+_strncat(np, buffer + a, b - a);
+else
+_strncpy(np, buffer + a, b - a + 1);
+
+e += b - a;
+a = b;
+f = np;
+
+if (curr_len)
+*curr_len = e;
+*pointer = f;
+return (e);
+}
