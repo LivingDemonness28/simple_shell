d9fb5adaa92d9e0adb46973e66b13dcadedd23f6
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-24 15:46:37 +0200

    functions_13.c: _lprint function added

--- functions_14.c
+++ functions_14.c
@@ -1,1 +1,22 @@
 #include "simpleshell.h"
+
+/**
+ * _lprint - prints all elements of a linked list.
+ * @head: head of list.
+ * Return: Num of nodes in list.
+*/
+size_t _lprint(const list_t *head)
+{
+size_t a = 0;
+
+for (; head; a++)
+{
+_puts(_itoa(head->number, 10, 0));
+_putchar(':');
+_putchar(' ');
+_puts(head->string ? head->string : "(nil)");
+_puts("\n");
+head = head->next;
+}
+return (a);
+}
