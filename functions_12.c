b32ab5c4de4cd2c8392938adca56b50675f49c43
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-24 04:29:13 +0200

    functions_12.c: _del_env function added

--- functions_12.c
+++ functions_12.c
@@ -14,3 +14,35 @@ ss_info->env_mod = 0;
 }
 return (ss_info->custom_env);
 }
+
+/**
+ * _del_env - unset an env var in the local
+ * env list.
+ * @ss_info: simpleshell_t struct param
+ * @envvar: environment variable/
+ * Return: 1 (Success), 0 (Otherwise)
+*/
+int _del_env(simpleshell_t *ss_info, char *envvar)
+{
+list_t *n = ss_info->local_env_list;
+size_t a = 0;
+char *es;
+
+if (!n || !envvar)
+return (0);
+
+for (; n; a++)
+{
+es = _starts(n->string, envvar);
+if (es && *es == '=')
+{
+ss_info->env_mod = _del_node_at_ind(&(ss_info->local_env_list), a);
+a = 0;
+n = ss_info->local_env_list;
+continue;
+}
+n = n->next;
+a++;
+}
+return (ss_info->env_mod);
+}
