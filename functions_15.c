17294dc66c121c8a1f196dd6ddc6b214a761b4d2
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-24 18:33:48 +0200

    functions_15.c: _updt_cd function added

--- functions_15.c
+++ functions_15.c
@@ -44,3 +44,54 @@ if (curr_len)
 *pointer = f;
 return (e);
 }
+
+/**
+ * _updt_cd = changes current directory
+ * @ss_info: simpleshell_t struct param.
+ * Return: 0 (Always)
+*/
+int _updt_cd(simpleshell_t *ss_info)
+int _mycd(simpleshell_t *ss_info)
+{
+char *path = getcwd(buffer, 1024), *tar_dir, buffer[1024];
+int res;
+
+if (!path)
+_puts("Error: Unable to retrieve current directory.\n");
+
+if (!ss_info->av[1])
+{
+tar_dir = _envval(ss_info, "SS_HOME=");
+if (!tar_dir)
+res = chdir((tar_dir = _envval(ss_info, "SS_PWD=")) ? tar_dir : "/");
+else
+res = chdir(tar_dir);
+}
+else if (_strcmp(ss_info->av[1], "-") == 0)
+{
+if (!_envval(ss_info, "SS_OLDPWD="))
+{
+_puts(path);
+_putchar('\n');
+return (1);
+}
+_puts(_envval(ss_info, "SS_OLDPWD="));
+_putchar('\n');
+res = chdir((tar_dir = _envval(ss_info, "SS_OLDPWD=")) ? tar_dir : "/");
+}
+else
+res = chdir(ss_info->av[1]);
+
+if (res == -1)
+{
+_eprint(ss_info, "can't cd to ");
+_w_str_stderr(ss_info->av[1]);
+_w_stderr('\n');
+}
+else
+{
+_updt_env(ss_info, "SS_OLDPWD", _envval(ss_info, "SS_PWD="));
+_updt_env(ss_info, "SS_PWD", getcwd(buffer, 1024));
+}
+return (0);
+}
