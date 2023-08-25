824e924c241e8fe25b67ff7e721c62420b180571
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-25 21:12:16 +0200

    functions_8.c: _unshift_node function updated

--- functions_8.c
+++ functions_8.c
@@ -39,45 +39,6 @@ return (NULL);
 
 if (!nn)
 return (NULL);
-
-_memset((void *)nn, 0, sizeof(list_t));
-nn->number = num;
-if (str)
-{
-nn->string = _strdup(str);
-if (!nn->string)
-{
-free(nn);
-return (NULL);
-}
-}
-if (n)
-{
-for (; n->next; n = n->next)
-;
-n->next = nn;
-}
-else
-*h = nn;
-return (nn);
-}
-
-/**
- * _unshift_node - adds node to start of list.
- * @h: head of list.
- * @str: string field of new node.
- * @num: node index.
- * Return: ptr to new node (Success), NULL (Otherwise).
-*/
-list_t *_unshift_node(list_t **h, const char *str, int num)
-{
-list_t *nn = malloc(sizeof(list_t)), *n = *h;
-
-if (!h)
-return (NULL);
-
-if (!nn)
-return (NULL);
 _memset((void *)nn, 0, sizeof(list_t));
 nn->number = num;
 if (str)
@@ -98,6 +59,37 @@ n->next = nn;
 else
 *h = nn;
 return (nn);
+}
+
+/**
+ * _unshift_node - adds node to start of list.
+ * @h: head of list.
+ * @str: string field of new node.
+ * @num: node index.
+ * Return: ptr to new node (Success), NULL (Otherwise).
+*/
+list_t *_unshift_node(list_t **h, const char *str, int num)
+{
+list_t *nh = malloc(sizeof(list_t));
+
+if (!h)
+return (NULL);
+if (!nh)
+return (NULL);
+_memset((void *)nh, 0, sizeof(list_t));
+nh->number = num;
+if (str)
+{
+nh->string = _strdup(str);
+if (!nh->string)
+{
+free(nh);
+return (NULL);
+}
+}
+nh->next = *h;
+*h = nh;
+return (nh);
 }
 
 /**
