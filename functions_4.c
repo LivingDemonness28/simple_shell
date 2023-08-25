5ed98edfbd31cf612dd88d3af9325ecc3b46a277
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-25 16:50:13 +0200

    functions_4.c: _strcat function updated

--- functions_4.c
+++ functions_4.c
@@ -33,21 +33,14 @@ return (dest);
 */
 char *_strcat(char *dest, char *src)
 {
-int i = 0, j = 0;
+char *res = dest;
 
-while (dest[i] != '\0')
-{
-i++;
-}
-
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
+while (*dest)
+dest++;
+while (*src)
+*dest++ = *src++;
+*dest = *src;
+return (res);
 }
 
 /**
