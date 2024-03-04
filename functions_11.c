49ccdb356e2b70bd94253515a69ef3d89b9a198c
LivingDemonness28 <ajamshamoodah@gmail.com>
2024-03-04 13:52:48 +0200

    functions_11.c file updated 7th

--- functions_11.c
+++ functions_11.c
@@ -54,7 +54,7 @@ return (d);
 */
 void _clear_shell(ss_t *ss_info, int all)
 {
-_free_str(ss_info->argv);
+_free_ptr((void **)&(ss_info->argv));
 ss_info->argv = NULL;
 ss_info->cmd_path = NULL;
 if (all)
